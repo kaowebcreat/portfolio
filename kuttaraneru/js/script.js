69da1c6cf6486d14663e2dc1760d723b5d126c4d
谷口夏央 <taniguchi_kao@ca-adv.co.jp>
2024-08-14 19:04:03 +0900

    #2 更新

--- kuttaraneru/js/script.js
+++ kuttaraneru/js/script.js
@@ -1,13 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hoverArea = document.querySelector('.hover-area');
-    const audio = document.getElementById('hover-audio');
+// 元の背景画像を設定
+const initialBg = '../img/back.png';
 
-    hoverArea.addEventListener('mouseenter', function() {
-        audio.currentTime = 0;  // 音楽を最初から再生
-        audio.play();
+// 背景画像をフェードインで変更し、ループを止めるスクリプト
+const bgLoop = document.querySelector('.bg-image-loop');
+
+
+document.querySelectorAll('.loop_wrap ul li').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        const newBg = item.getAttribute('data-bg');
+        bgLoop.style.backgroundImage = `url('${newBg}')`;
+        bgLoop.style.backgroundSize = '110%'; // 切り替え時は cover
+        bgLoop.style.width = '100vw';
+        bgLoop.style.backgroundRepeat = 'repeat';
+        bgLoop.style.animationPlayState = 'none'; // ループを止める
     });
 
-    hoverArea.addEventListener('mouseleave', function() {
-        audio.pause();  // 音楽を停止
+    item.addEventListener('mouseout', () => {
+        bgLoop.style.backgroundImage = `url('${initialBg}')`; // 元の背景に戻す
+        bgLoop.style.animationPlayState = 'running'; // ループを再開
+        bgLoop.style.width = '10000px';
+    });
+});
+
+// script.js
+$(document).ready(function() {
+    var modal1 = $('#modal1');
+    var span = $('.close');
+    var loop = $('.bg-image-loop');
+
+    // モーダルを開く
+    $('.modal1_open').click(function() {
+        loop.fadeOut();
+        modal1.fadeIn();
+    });
+
+    // モーダルを閉じる
+    span.click(function() {
+        modal1.hide();
+    });
+
+    // モーダルの外側をクリックすると閉じる
+    $(window).click(function(event) {
+        if ($(event.target).is(modal1)) {
+            modal1.hide();
+        }
     });
 });
